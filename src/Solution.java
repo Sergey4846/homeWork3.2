5a467358185e215ab102cae658d560d8a60b5a1d
Sergey4846 <sergeyruzhankov@gmail.com>
2019-04-18 14:55:32 +0300

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println("Введите число = " + a);
+
+        if ((a % 10) == 7) {
+            System.out.println("Последняя цифра является семёркой");
+        }
+
+        else
+
+            System.out.println("Последняя цифра не является семёркой");
+        }
+
+
+
+
+    }
